e27ae6a6c70a863f7c2940667f92437a37e0adc4
denn-is-njeruh <dennis.njeru@student.moringaschool.com>
2021-10-05 08:16:12 +0300

    add thome and shome view functions for teacher and student apps

--- student/views.py
+++ student/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from customusers.models import user_type
 
 # Create your views here.
+def shome(request):
+    if request.user.is_authenticated and user_type.objects.get(user=request.user).is_student:
+        return render(request,'student_home.html')
+    elif request.user.is_authenticated and user_type.objects.get(user=request.user).is_teach:
+        return redirect('thome')
+    else:
+        return redirect('login')
--- teacher/views.py
+++ teacher/views.py
@@ -1,4 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from customusers.models import user_type
 
 # Create your views here.
+def thome(request):
+    if request.user.is_authenticated and user_type.objects.get(user=request.user).is_teach:
+        return render(request, 'teacher_home.html')
+    elif request.user.is_authenticated and user_type.objects.get(user=request.user).is_student:
+        return redirect('shome')
+    else: 
+        return redirect('home')
 
