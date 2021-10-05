a6f0d58183b01efe626e33842fa62388e08cb61a
denn-is-njeruh <dennis.njeru@student.moringaschool.com>
2021-10-05 08:33:12 +0300

    register teacher and student apps to settings

--- multipleusers/settings.py
+++ multipleusers/settings.py
@@ -41,6 +41,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'customusers.apps.CustomusersConfig',
+    'student.apps.StudentConfig',
+    'teacher.apps.TeacherConfig',
 ]
 
 MIDDLEWARE = [
